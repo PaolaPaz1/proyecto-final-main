54d778e9c28a21bd81572c67f0af925abfe5b8ed
Rosendo Garcia <grosendoh73@gmail.com>
2023-11-03 07:59:19 -0600

    aplicar configuracion con dotenv

--- api/database.js
+++ api/database.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise'
+import { config } from 'dotenv'
+
+config()
 
 const db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'users_crud_php'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.NODE_ENV === 'test' ? process.env.DB_TEST_NAME : process.env.DB_NAME
 })
 
 export default db
