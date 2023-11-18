f2f4c542a22fdba76564d0f872731084325bdd41
Rosendo Garcia <grosendoh73@gmail.com>
2023-11-18 14:06:28 -0600

    mejorar logica de errores al controller de expenses

--- api/controllers/expenseController.js
+++ api/controllers/expenseController.js
@@ -1,80 +1,79 @@
 import Expense from '../models/expense.js'
 
 class ExpenseController {
-  async newExpense (req, res) {
-    const { userId, amount, description, category, date } = req.body
-
+  async newExpense (req, res, next) {
     try {
+      const { userId, amount, description, category, date } = req.body
+      if (!userId || !amount || !description || !category || !date) return next(new Error('Todos los campos son requeridos'))
       await Expense.newExpense(userId, amount, description, category, date)
       res.json({ message: 'Egreso creado' })
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: err.message })
+      next(err)
     }
   }
 
-  async getExpenses (req, res) {
-    const { userId, year, month } = req.body
-
+  async getExpenses (req, res, next) {
     try {
+      const { userId, year, month } = req.body
+      if (!userId || !year || !month) return next(new Error('Todos los campos son requeridos'))
+
       const [expenses] = await Expense.getExpenses(userId, year, month)
 
       res.json(expenses)
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: err.message })
+      next(err)
     }
   }
 
-  async getLimitedExpenses (req, res) {
-    const { userId, year, month } = req.body
-
+  async getLimitedExpenses (req, res, next) {
     try {
+      const { userId, year, month } = req.body
+      if (!userId || !year || !month) return next(new Error('Todos los campos son requeridos'))
+
       const [expenses] = await Expense.getLimitedExpenses(userId, year, month)
 
       res.json(expenses)
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: err.message })
+      next(err)
     }
   }
 
-  async getTotalExpenses (req, res) {
-    const { userId, year, month } = req.body
-
+  async getTotalExpenses (req, res, next) {
     try {
+      const { userId, year, month } = req.body
+      if (!userId || !year || !month) return next(new Error('Todos los campos son requeridos'))
+
       const [expenses] = await Expense.getTotalExpenses(userId, year, month)
 
       res.json(expenses)
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: err.message })
+      next(err)
     }
   }
 
-  async getExpensesByCategory (req, res) {
-    const { userId, year, month } = req.body
-
+  async getExpensesByCategory (req, res, next) {
     try {
+      const { userId, year, month } = req.body
+      if (!userId || !year || !month) return next(new Error('Todos los campos son requeridos'))
+
       const [expenses] = await Expense.getExpensesByCategory(userId, year, month)
 
       res.json(expenses)
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: err.message })
+      next(err)
     }
   }
 
-  async checkMonthlyLimitExp (req, res) {
-    const { userId, year, month } = req.body
-
+  async checkMonthlyLimitExp (req, res, next) {
     try {
+      const { userId, year, month } = req.body
+      if (!userId || !year || !month) return next(new Error('Todos los campos son requeridos'))
+
       const result = await Expense.checkMonthlyLimitExp(userId, year, month)
 
       res.json(result)
     } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: err.message })
+      next(err)
     }
   }
 }
